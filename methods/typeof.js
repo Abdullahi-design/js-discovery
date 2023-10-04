527d3fb58c673963e0aefc83aad5f5fa7fc58bc1
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-10-04 18:30:08 +0100

    typeof methos

---
+++ methods/typeof.js
@@ -0,0 +1,12 @@
+let man = 'Abdullahi';
+
+// check if this is a type of string
+if(typeof man === 'string'){
+
+    console.log(`${man} Sani`);
+
+}else{
+
+    console.log('not a string');
+    
+}
